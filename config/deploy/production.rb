a2fc11f9f94eb7e1d96290f319dde4b6f7c3cba5
Vicente Castro <vocastro@uc.cl>
2019-04-30 10:41:57 -0400

    cambio user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "tuerca4.ing.puc.cl", user: "administrator", roles: %w{app db web}
+server "tuerca4.ing.puc.cl", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
