b68b6e60e4a166d27d1793b33dc0c5bdf0067023
Vicente Castro <vocastro@uc.cl>
2019-04-30 19:35:18 -0400

    delete sidekiq autostart

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,19 +64,19 @@ namespace :deploy do
   end
 end
 
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :systemctl, :restart, :sidekiq
-    end
-  end
-end
+#namespace :sidekiq do
+#  task :quiet do
+#    on roles(:app) do
+#      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
+#    end
+#  end
+#  task :restart do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :restart, :sidekiq
+#    end
+#  end
+#end
 
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
+#after 'deploy:starting', 'sidekiq:quiet'
+#after 'deploy:reverted', 'sidekiq:restart'
+#after 'deploy:published', 'sidekiq:restart'
