de18ec590ec2316b6e76f94c023672335db273af
Vicente Castro <vocastro@uc.cl>
2019-04-30 11:22:31 -0400

    added production secret_key_base

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,4 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :tmp_dir, "/home/deploy"
