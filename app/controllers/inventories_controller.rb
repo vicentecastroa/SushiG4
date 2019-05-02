37021842fb8aea90ced49bfccb110b6f6b0a83ed
Vicente Castro <vocastro@uc.cl>
2019-05-02 09:19:57 -0400

    cambio id almacen get inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -38,7 +38,7 @@ class InventoriesController < ApplicationController
 	end
 
 	def show_inventory
-		@request = (obtener_skus_con_stock(@@api_key, "5cbd3ce444f67600049431c6")).to_a
+		@request = (obtener_skus_con_stock(@@api_key, "5cc7b139a823b10004d8e6e0")).to_a
 		response = []
 		for element in @request do
 			sku = element["_id"]
