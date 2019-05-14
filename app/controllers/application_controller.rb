96c1e1c238a0b29dd81a14968da1dc1d110b012c
Carlos Fuentes <cafuentes1@uc.cl>
2019-05-14 10:56:31 -0400

    Función mover a cocina y ids dev y prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,40 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	@@api_key = "o5bQnMbk@:BxrE"
+
+	#IDs Producción
 	@@id_recepcion = "5cc7b139a823b10004d8e6df"
 	@@id_despacho = "5cc7b139a823b10004d8e6e0"
 	@@id_pulmon = "5cc7b139a823b10004d8e6e3"
 	@@id_cocina = "5cc7b139a823b10004d8e6e4"
+	@@url = "https://integracion-2019-prod.herokuapp.com/bodega"
+
+	#IDs Desarrollo
+	#@@id_recepcion = "5cbd3ce444f67600049431c5"
+	#@@id_despacho = "5cbd3ce444f67600049431c6"
+	#@@id_pulmon = "5cbd3ce444f67600049431c9"
+	#@@id_cocina = "5cbd3ce444f67600049431ca"
+	#@@url = "https://integracion-2019-dev.herokuapp.com/bodega"
 
 	@@print_valores = false
+
+	# Capacidades Bodegas
+	@@tamaño_cocina = 1122
+	@@tamaño_recepcion = 133
+	@@tamaño_despacho = 80
+	@@tamaño_pulmon = 99999999
 
 	def hashing(data, api_key)
 		hmac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), api_key.encode("ASCII"), data.encode("ASCII"))
 		signature = Base64.encode64(hmac).chomp
 		return signature
-  	end
+  end
 
-  	# Funcionando bien
-  	def get_almacenes(api_key)
+  # Funcionando bien
+  def get_almacenes(api_key)
 		data = "GET"
 		hash_value = hashing(data, api_key)
-		almacenes = HTTParty.get('https://integracion-2019-prod.herokuapp.com/bodega/almacenes', 
+		almacenes = HTTParty.get("#{@@url}/almacenes", 
 		  headers:{
 		    "Authorization": "INTEGRACION grupo4:#{hash_value}",
 		    "Content-Type": "application/json"
@@ -35,7 +51,7 @@ class ApplicationController < ActionController::Base
 	def get_products_from_almacenes(api_key, almacenId, sku)
 		data = "GET#{almacenId}#{sku}"
 		hash_value = hashing(data, api_key)
-		products = HTTParty.get("https://integracion-2019-prod.herokuapp.com/bodega/stock?almacenId=#{almacenId}&sku=#{sku}",
+		products = HTTParty.get("#{@@url}/stock?almacenId=#{almacenId}&sku=#{sku}",
 		  headers:{
 		    "Authorization": "INTEGRACION grupo4:#{hash_value}",
 		    "Content-Type": "application/json"
@@ -51,7 +67,7 @@ class ApplicationController < ActionController::Base
 	def get_products_from_almacenes_limit_primeros(api_key, almacenId, sku, limit)
 		data = "GET#{almacenId}#{sku}"
 		hash_value = hashing(data, api_key)
-		products = HTTParty.get("https://integracion-2019-prod.herokuapp.com/bodega/stock?almacenId=#{almacenId}&sku=#{sku}&limit=#{limit}",
+		products = HTTParty.get("#{@@url}/stock?almacenId=#{almacenId}&sku=#{sku}&limit=#{limit}",
 		  headers:{
 		    "Authorization": "INTEGRACION grupo4:#{hash_value}",
 		    "Content-Type": "application/json"
@@ -68,7 +84,7 @@ class ApplicationController < ActionController::Base
   	def mover_producto_entre_bodegas(api_key, productoId, almacenId, oc, precio)
 		data = "POST#{productoId}#{almacenId}"
 		hash_value = hashing(data, api_key)
-		producto_movido = HTTParty.post('https://integracion-2019-prod.herokuapp.com/bodega/moveStockBodega',
+		producto_movido = HTTParty.post("#{@@url}/moveStockBodega",
 		  body:{
 		  	"productoId": productoId,
 		  	"almacenId": almacenId,
@@ -94,7 +110,7 @@ class ApplicationController < ActionController::Base
 
 		data = "POST#{productoId}#{almacenId}"
 		hash_value = hashing(data, @@api_key)
-		req = HTTParty.post("https://integracion-2019-prod.herokuapp.com/bodega/moveStock",
+		req = HTTParty.post("#{@@url}/moveStock",
 		  body:{
 				"productoId": productoId,
 				"almacenId": almacenId,
@@ -116,7 +132,7 @@ class ApplicationController < ActionController::Base
 	def obtener_skus_con_stock(api_key, almacenId)
 		data = "GET#{almacenId}"
 		hash_value = hashing(data, api_key)
-		skus = HTTParty.get("https://integracion-2019-prod.herokuapp.com/bodega/skusWithStock?almacenId=#{almacenId}",
+		skus = HTTParty.get("#{@@url}/skusWithStock?almacenId=#{almacenId}",
 		  headers:{
 		    "Authorization": "INTEGRACION grupo4:#{hash_value}",
 		    "Content-Type": "application/json"
@@ -132,7 +148,7 @@ class ApplicationController < ActionController::Base
 	def fabricar_sin_pago(api_key, sku, cantidad)
 		data = "PUT#{sku}#{cantidad}"
 		hash_value = hashing(data, api_key)
-		products_produced = HTTParty.put("https://integracion-2019-prod.herokuapp.com/bodega/fabrica/fabricarSinPago",
+		products_produced = HTTParty.put("#{@@url}/fabrica/fabricarSinPago",
 		  body:{
 		  	"sku": sku,
 		  	"cantidad": cantidad
@@ -158,6 +174,89 @@ class ApplicationController < ActionController::Base
 			end
 		end
 		puts "..................."
+	end
+
+	## NEW ##
+
+	def checkear_recepcion(api_key)
+		
+		puts "Comienzo función checkear_recepcion\n"
+
+		# Obtenemos espacio en Cocina
+		almacenes = (get_almacenes(api_key)).to_a
+		for almacen in almacenes do
+			if almacen["cocina"] == true
+				puts "Almacen usedSpace: " + almacen["usedSpace"].to_s
+				if almacen["usedSpace"] <= almacen["totalSpace"]
+					@espacio_disponible = almacen["totalSpace"] - almacen["usedSpace"]
+					puts "Espacio disponible en cocina: " + @espacio_disponible.to_s + "\n"
+
+					puts "Vaciando Pulmón"
+
+					# Obtenemos los skus en Pulmon
+					skus_pulmon = obtener_skus_con_stock(api_key, @@id_pulmon)
+
+					# Para cada sku, obtenemos productos
+					for sku_pulmon in skus_pulmon
+						puts "SKU en Pulmón: " + sku_pulmon["_id"]
+						sku_pulmon_num = sku_pulmon["_id"]
+
+						# Obtenemos los productos asociados a ese sku
+						productos_sku_pulmon_num = get_products_from_almacenes(api_key, @@id_pulmon, sku_pulmon_num)
+
+						# Movemos cada producto de Pulmon a Cocina hasta que:
+						# 1. No haya mas productos de ese sku
+						# 2. Se llene la cocina
+						for prod in productos_sku_pulmon_num
+							if @espacio_disponible == 0
+								puts "Cocina Llena\n"
+								return
+							end			
+							mover_producto_entre_almacenes(prod["_id"], @@id_cocina)
+							puts "Producto movido de Pulmón a Cocina"
+
+							# Disminuyo en 1 el espacio disponible
+							@espacio_disponible -= 1
+						end
+					end
+
+					puts "Pulmón Vaciado"
+
+					puts "Vaciando Recepción"
+
+					# Obtenemos los skus en Recepción
+					skus_recepcion = obtener_skus_con_stock(api_key, @@id_recepcion)
+
+					# Para cada sku, obtenemos productos
+					for sku_recepcion in skus_recepcion
+						puts "SKU en Recepcion: " + sku_recepcion["_id"]
+						sku_recepcion_num = sku_recepcion["_id"]
+
+						# Obtenemos los productos asociados a ese sku
+						productos_sku_recepcion_num = get_products_from_almacenes(api_key, @@id_recepcion, sku_recepcion_num)
+
+						# Movemos cada producto de Pulmon a Cocina hasta que:
+						# 1. No haya mas productos de ese sku
+						# 2. Se llene la cocina
+						for prod in productos_sku_recepcion_num
+							if @espacio_disponible == 0
+								puts "Cocina Llena\n"
+								return
+							end
+							
+							mover_producto_entre_almacenes(prod["_id"], @@id_cocina)
+							puts "Producto movido de Recepción a Cocina"
+
+							# Disminuyo en 1 el espacio disponible
+							@espacio_disponible -= 1
+						end
+					end
+
+					puts "Recepción Vaciada"
+
+				end
+			end
+		end		
 	end
 
   protect_from_forgery with: :exception
