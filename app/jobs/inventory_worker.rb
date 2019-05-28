56758194e0c108a5c3f5dfbe2c1d924b79721c2b
Carlos Fuentes <cafuentes1@uc.cl>
2019-05-28 19:46:13 -0400

    solicitar_orden en inventory_worker

--- app/jobs/inventory_worker.rb
+++ app/jobs/inventory_worker.rb
@@ -68,7 +68,7 @@ class InventoryWorker < ApplicationJob
 
 				# Si el producto es MASAGO, lo pido a los grupos productores correspondientes
 				if sku.to_i == 1013
-					#get_producto_grupo(1013, cantidad_faltante)
+					puts solicitar_orden("1013", cantidad_a_producir)
 					#break #cambio a revisar al siguiente producto de p_minimos
 
 				# Si el producto NO es MASAGO, debo verificar el stock de sus ingredientes antes de fabricar
@@ -129,7 +129,7 @@ class InventoryWorker < ApplicationJob
 
 							# Si el producto no es nuestro, lo pedimos a otro grupo
 							else
-
+								solicitar_orden(ingrediente.ingrediente_id, cantidad_a_producir_ingrediente)
 								#get_producto_grupo(p_ingrediente.sku, cantidad_faltante_ingrediente)
 
 							end
